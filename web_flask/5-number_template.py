61c53f4e5e520f1b046af2af2d2cd7dde757ddca
Dawitt-dev <davoza27@yahoo.com>
2024-04-24 17:46:29 -0700

    task_5.Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """Displays HTML page with  H1 tag containing 'Number: n' if n is an int"""
-        return render_template('5-number.html', value=n)
+        return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
